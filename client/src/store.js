ba44f9f07b3ab7474994c16429cc6dbc293d64b7
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-06-26 08:18:53 +0200

    fixed blank screen on build

--- client/src/store.js
+++ client/src/store.js
@@ -2,15 +2,16 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/index"
 
+// Dev env
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 const initState = {};
-const middleware = [thunk]
 
 const store = createStore(
     rootReducer,
     initState,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk),
     ));
 
 
